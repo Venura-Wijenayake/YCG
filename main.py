eaa69263dfe7d05f60992071409d0102f7304a5d
Venura Wijenayake <venu.wije@gmail.com>
2025-07-12 15:46:00 -0700

    .Env and keys are now set up - Checkpoint 0.2

---
+++ main.py
@@ -0,0 +1,1 @@
+from utils.config import OPENAI_API_KEY
---
+++ utils/config.py
@@ -0,0 +1,11 @@
+# utils/config.py
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+
+
+print("OPENAI_API_KEY:", OPENAI_API_KEY)
+print("GITHUB_TOKEN:", GITHUB_TOKEN)
